6e36862ec2b6f0978dfd828c046b78252e8a4759
akbar936 <akbarkharisma000@gmail.com>
2024-12-25 14:06:31 +0700

    menambahkan fungsi garis

--- 01_helloWord.cpp
+++ 01_helloWord.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
 using namespace std;
 
+void iniGaris()
+{
+    cout << "=============================\n";
+}
+
+
 int main()
 {
+    iniGaris();
     cout << "Hello World\n";
+    iniGaris();
     return 0;
 }
